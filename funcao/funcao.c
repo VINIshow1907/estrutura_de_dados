f073b3ec0650955031178ed3bff5c69856ecd55f
VINIshow1907 <viniciuslucio792@gmail.com>
2024-03-19 21:36:06 -0300

    Exercicio do menu usando função

---
+++ funcao/funcao.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void menu();
+
+int main(){
+    menu();
+    
+    if (fatorial = numero)
+}
+void menu(){
+    int numero;
+    printf("===============MENU===============\n");
+    printf("ESCOLHA [1] PARA FAZER O FATORIAL\n");
+    printf("ESCOLHA [2] PARA VERIFICAR O NUMERO PRIMO \n");
+    printf("ESCOLHA [3] PARA FAZER X ELEVADO A Y \n");
+    printf("ESCOLHA [4] PARA FAZER A TABUADA DELE \n");
+    printf("ESCOLHA [0] PARA SAIR \n");
+
+    printf("Digite um número de qual serviço você quer realizar!\n");
+    scanf("%d", &numero);
+
+}
